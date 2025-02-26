244b65fa773c73a730d532402fb5246a818b1adb
nguyen <nguyenthcs430@gmail.com>
2025-02-26 21:41:21 +0700

    login, logout, register

--- src/components/SignInForm.vue
+++ src/components/SignInForm.vue
@@ -1,58 +1,69 @@
 <template>
-  <Form >
+  <Form @submit="submitForm">
     <div class="form-group">
-      <Field name="username" type="text" class="form-control" placeholder="Nhập tên đăng nhập" />
+      <Field
+        name="username"
+        type="text"
+        class="form-control"
+        placeholder="Nhập tên đăng nhập"
+      />
     </div>
 
     <div class="form-group">
-      <Field name="password" type="password" class="form-control" placeholder="Nhập mật khẩu" />
+      <Field
+        name="password"
+        type="password"
+        class="form-control"
+        placeholder="Nhập mật khẩu"
+      />
     </div>
 
     <div class="form-group">
-       <button class="btn_login">
-        Đăng nhập
-      </button>
-      
-      
+      <button class="btn_login">Đăng nhập</button>
     </div>
   </Form>
 </template>
 
 <script>
-import { Form, Field } from 'vee-validate'; // Đảm bảo import đúng
-
+import { Form, Field } from "vee-validate"; // Đảm bảo import đúng
+import { useAuthStore } from "@/stores/auth";
 export default {
   components: {
     Form,
-    Field
+    Field,
   },
-  data() {
-   
-  },
+  data() {},
   methods: {
-    submitForm(values) {
+    async submitForm(values) {
+      const authStore = useAuthStore();
+
+      await authStore
+        .login(values)
+        .then(() => {
+          this.$router.replace({ name: "main" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
       // Xử lý khi form được submit, values chứa các giá trị từ các trường
       console.log(values);
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style >
+<style>
 .form-group {
   margin-bottom: 15px;
 }
-
-
 
 .form-group input {
   width: 100%;
   padding: 8px;
   font-size: 10px;
-
 }
 
-  .btn_login {
+.btn_login {
   width: 200px;
   height: 40px;
   border: 2px solid #fff;
@@ -68,5 +79,4 @@ export default {
   background: white;
   color: #6f5fe2;
 }
-
 </style>
--- src/plugins/authetication.ts
+++ src/plugins/authetication.ts
@@ -1,13 +1,13 @@
-// import { useAuthStore } from '../stores/auth'
+import { useAuthStore } from './../stores/auth';
 
-// export const authentication = {
-//   async install() {
-//     const store = useAuthStore()
-//     try {
-//       await store.attempt()
-//       return
-//     } catch (error) {
-//       return
-//     }
-//   }
-// }
+export const authentication = {
+  async install() {
+    const store = useAuthStore()
+    try {
+      await store.attempt()
+      return
+    } catch (error) {
+      return
+    }
+  }
+}
